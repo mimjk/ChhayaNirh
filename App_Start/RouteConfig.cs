c85f724b37ddc9b6721fd7ebc0b59f2c79430f4a
Ishrat Jahan Mim <141272899+mimjk@users.noreply.github.com>
2025-09-05 15:59:04 +0600

    Terms & Conditions and CSS

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -13,6 +13,43 @@ namespace ChhayaNirh
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            // Custom clean routes
+            routes.MapRoute(
+                name: "Login",
+                url: "Login",
+                defaults: new { controller = "Login", action = "Login" }
+            );
+
+            routes.MapRoute(
+                name: "Signup",
+                url: "Signup",
+                defaults: new { controller = "Signup", action = "Signup" }
+            );
+
+            routes.MapRoute(
+                name: "Chat",
+                url: "Chat",
+                defaults: new { controller = "Chat", action = "Chat" }
+            );
+
+            routes.MapRoute(
+                name: "Post",
+                url: "Post",
+                defaults: new { controller = "Post", action = "Post" }
+            );
+
+            routes.MapRoute(
+                name: "Profile",
+                url: "Profile",
+                defaults: new { controller = "Profile", action = "Profile" }
+            );
+
+            routes.MapRoute(
+                name: "Terms",
+                url: "Terms",
+                defaults: new { controller = "Terms", action = "Terms" }
+            );
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
