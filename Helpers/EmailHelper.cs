402b4838af333996897fe3be217f4ba896178951
Ishrat Jahan Mim <141272899+mimjk@users.noreply.github.com>
2025-09-08 16:19:41 +0600

    Email Verification Code done

---
+++ Helpers/EmailHelper.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace ChhayaNirh.Helpers
+{
+    public static class EmailHelper
+    {
+        // Gmail App Password ব্যবহার করে email পাঠানো
+        public static void SendEmail(string toEmail, string subject, string body)
+        {
+            var fromAddress = new MailAddress("afiasiddiquemim@gmail.com", "ChhayaNirh"); // তোমার Gmail
+            var toAddress = new MailAddress(toEmail);
+            const string fromPassword = "ljtd ltdt telb vvur"; // 16-character App Password
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            })
+            {
+                smtp.Send(message);
+            }
+        }
+    }
+}
---
+++ Migrations/202509080743478_AddEmailVerificationToUser.cs
@@ -0,0 +1,22 @@
+﻿namespace ChhayaNirh.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddEmailVerificationToUser : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Users", "IsEmailVerified", c => c.Boolean(nullable: false));
+            AddColumn("dbo.Users", "EmailVerificationCode", c => c.String());
+            AddColumn("dbo.Users", "EmailVerificationExpiry", c => c.DateTime());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Users", "EmailVerificationExpiry");
+            DropColumn("dbo.Users", "EmailVerificationCode");
+            DropColumn("dbo.Users", "IsEmailVerified");
+        }
+    }
+}
